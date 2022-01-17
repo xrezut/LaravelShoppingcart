edb82206413c6d628096e38a5eb132f6c517d7c7
Patrick <privat@skyraptor.eu>
2022-01-18 00:25:15 +0100

    Update Buyable.php

--- src/Contracts/Buyable.php
+++ src/Contracts/Buyable.php
@@ -9,26 +9,26 @@ interface Buyable
      *
      * @return int|string
      */
-    public function getBuyableIdentifier($options = null);
+    public function getBuyableIdentifier();
 
     /**
      * Get the description or title of the Buyable item.
      *
      * @return string
      */
-    public function getBuyableDescription($options = null);
+    public function getBuyableDescription();
 
     /**
      * Get the price of the Buyable item.
      *
      * @return float
      */
-    public function getBuyablePrice($options = null);
+    public function getBuyablePrice();
 
     /**
      * Get the weight of the Buyable item.
      *
      * @return float
      */
-    public function getBuyableWeight($options = null);
+    public function getBuyableWeight();
 }
